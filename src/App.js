5ef25a80c1ea4ef83481f2bc8252798834b2fbf3
Juliana Cotabarren <julianacotabarren@gmail.com>
2023-05-03 10:26:35 -0300

    peticiones

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import { useEffect } from 'react';
 import './App.css';
-import ProductsContainer from './components/ProductsContainer';
-import EcommerceProvider from './context/EcommerceProvider';
+import axios from 'axios';
+
 
 function App() {
+  useEffect(() => {
+    const getAllPosts = () => {
+
+      //Petición Sincrónica
+      fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response)=> response.json())
+      .then((json) => console.log(json))
+      .catch((err) => console.log(err))
+    }
+    getAllPosts()
+  
+    const getPost = async (id) => {
+      try {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getPost(1)
+
+  }, [])
+  
+
+
   return (
     <div className="App">
-      <EcommerceProvider>
-        <ProductsContainer/>
-      </EcommerceProvider>
+      
     </div>
   );
 }
